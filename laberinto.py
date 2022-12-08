e0d8c1c2e4f93d99987d1fbac897be129179f4d6
ferniprogrammer <fsanchor@myuax.com>
2022-12-08 18:15:24 +0100

    Terminados todos los movimientos

--- laberinto.py
+++ laberinto.py
@@ -24,3 +24,91 @@ if input() == "abajo":
 else:
      print("movimiento no válido, empieza de nuevo")
      quit
+     
+imprimir_laberinto(laberinto)
+print("segundo movimiento, estas en la posición (2, 3)")
+if input() == "abajo":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("tercer movimiento, estas en la posición (2, 4)")
+if input() == "abajo":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("cuarto movimiento, estas en la posición (2, 5)")
+if input() == "derecha":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("quinto movimiento, estas en la posición (3, 5)")
+if input() == "derecha":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("sexto movimiento, estas en la posición (4, 5)")
+if input() == "arriba":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("séptimo movimiento, estas en la posición (4, 4)")
+if input() == "arriba":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("octavo movimiento, estas en la posición (4, 3)")
+if input() == "derecha":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("noveno movimiento, estas en la posición (5, 3)")
+if input() == "derecha":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("décimo movimiento, estas en la posición (6, 3)")
+if input() == "abajo":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("penúltimo movimiento, estas en la posición (6, 4)")
+if input() == "abajo":
+     print("movimiento correcto, sigue así")
+else:
+     print("movimiento no válido, empieza de nuevo")
+     quit
+
+imprimir_laberinto(laberinto)
+print("último movimiento, estas en la posición (6, 5)")
+if input() == "abajo":
+    print("Lo lograstes, llegastes al final")
+else:
+    print("Movimiento no válido, empieza de nuevo")
+    quit
